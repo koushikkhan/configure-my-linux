3507bb4b62af05ae8b50e3f4b5a22c208f40035c
Koushik Khan <koushikkhan38@gmail.com>
2021-09-17 12:37:43 +0530

    added battery-conservation.sh
    script to control battery conservation mode in linux for Lenovo Ideapad laptops

---
+++ setup-pop-os/battery-conservation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+## Check for root user
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be executed as root"
+	exit 1
+fi
+
+
+## Check current status of battery conservation mode
+currentStatus=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
+if [ $currentStatus == 1 ]; then
+	echo "battery conservation is currently enabled"
+	echo -e "Would you like to disable now? Give 'yes' or 'no' as response\n"
+
+	read userInput
+
+	if [ $userInput == 'yes' ]; then
+		echo 0 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+		echo -e "\n battery conservation mode disabled successfully"
+	else
+		exit 0
+	fi;
+else
+	echo "battery conservation is currently disabled"
+	echo -e "Would you like to enable now? Give 'yes' or 'no' as response\n"
+
+	read userInput
+
+	if [ $userInput == 'yes' ]; then
+		echo 1 > /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode
+		echo -e "\n battery conservation mode enabled successfully"
+	else
+		exit 0
+	fi;
+fi;
