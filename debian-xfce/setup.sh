a489b6928d402161e5428dbbd1b4f5fdd0329b93
Koushik Khan <koushikkhan38@gmail.com>
2021-09-23 19:37:15 +0530

    code improved

--- debian-xfce/setup.sh
+++ debian-xfce/setup.sh
@@ -15,35 +15,48 @@ echo -e "\n\n"
 
 ## Define System Variables
 RESOURCE_DIR="$PWD/../resources"
+DEB_PKG_DIR="$RESOURCE_DIR/deb-pkgs"
 FONTS_DIR="$RESOURCE_DIR/fonts"
 CURSOR_THEMES_DIR="$RESOURCE_DIR/cursor-themes"
 BACKGROUNDS_DIR="$RESOURCE_DIR/backgrounds"
-PYTHON_INSTALL_DIR="/home/$SUDO_USER/miniconda"
-DEB_PKG_DIR="$RESOURCE_DIR/deb-pkgs"
-THEMES_DIR="$RESOURCE_DIR/themes"
 ICONS_DIR="$RESOURCE_DIR/icons"
+THEMES_DIR="$RESOURCE_DIR/themes"
+PYTHON_INSTALL_DIR="/home/$SUDO_USER/miniconda"
+
+
+## Setup temporary download directory
+echo -e "---> creating temporary directory for setup"
+if [ ! -d "$RESOURCE_DIR/tmp" ]; then
+	mkdir -p $RESOURCE_DIR/tmp
+fi
+echo -e "\n\n"
 
 
 ## Turn off system beep sound on backspace and/or logout
+echo -e "---> blacklisting pcspkr"
 echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
 
 
 ## Copy redshift configuration file
+echo -e "---> copying redshift configuration file"
 cp $RESOURCE_DIR/conf-files/redshift.conf /home/$SUDO_USER/.config/
 
 
 ## Add 'non-free' and 'contrib' sources
+echo -e "---> setting up additional package sources (for Debian only)"
 apt-get install -y software-properties-common && \
 add-apt-repository "contrib" && \
 add-apt-repository "non-free"
 
 
 ## Update repositories
+echo -e "---> updating package cache"
 apt-get -y update
 echo -e "\n\n"
 
 
 ## Install some required packages from repo
+echo -e "---> installing packages from repository"
 apt-get install -y git tlp tlp-rdw powertop \
 fonts-roboto curl wget gparted stacer micro timeshift \
 p7zip-full p7zip-rar rar unrar parole htop neofetch \
@@ -57,6 +70,7 @@ echo -e "\n\n"
 
 
 # Install additional packages offline
+echo -e "---> installing packages from local .deb files"
 for file in $DEB_PKG_DIR/*.deb; do
 	apt-get install -y $file
 done
@@ -64,33 +78,38 @@ apt-get -f install
 
 
 ## Install additional fonts
+echo -e "---> installing additional fonts"
 cp -r $FONTS_DIR/* /usr/share/fonts/ && \
 fc-cache -f -v
 echo -e "\n\n"
 
 
-## Install cursor themes
+## Install additional cursor themes
+echo -e "---> installing additional cursor icons"
 for file in $CURSOR_THEMES_DIR/*.tar.*; do 
 	tar -xvf "$file" -C /usr/share/icons/
 done
 echo -e "\n\n"
 
 
-# Install additional desktop background images
+# Install additional desktop wallpapers
+echo -e "---> installing additional desktop wallpaers"
 for file in $BACKGROUNDS_DIR/*; do 
 	cp $file /usr/share/images/desktop-base/
 done
 echo -e "\n\n"
 
 
-## Setup temporary download directory
-if [ ! -d "$RESOURCE_DIR/tmp" ]; then
-	mkdir -p $RESOURCE_DIR/tmp
-fi
+## Install additional icon packs
+echo -e "---> installing additional icon packs"
+for file in $ICONS_DIR/*.tar.*; do 
+	tar -xvf "$file" -C /usr/share/icons/
+done
 echo -e "\n\n"
 
 
 ## Install miniconda python
+echo -e "---> setting up miniconda python"
 if [ ! -f "$RESOURCE_DIR/tmp/miniconda.sh" ]; then
 	# echo -e "\nINFO: downloading miniconda installer script ...\n"
 	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh \
@@ -164,22 +183,25 @@ echo -e "\n\n"
 
 
 ## Perform upgrade
-apt-get -y dist-upgrade
+echo -e "---> looking for upgrade"
+apt-get -y upgrade && apt-get -y dist-upgrade
 
 
 ## Clean apt cache
+echo -e "---> cleaning apt cache"
 apt-get -y clean
 echo -e "\n\n"
 
 
 ## Remove redundant dependencies
+echo -e "---> cleaning abandoned package dependencies"
 apt-get -y autoremove
 echo -e "\n\n"
 
 
 ## Remove resources/tmp directory
-echo -e "Deleting $RESOURCE_DIR/tmp\n\n"
-rm -rf $RESOURCE_DIR/tmp;
+echo -e "---> deleting $RESOURCE_DIR/tmp\n\n"
+rm -rf $RESOURCE_DIR/tmp
 
 
 echo "# ------ Initial system configuration is now complete! ------ #"
